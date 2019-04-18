dee000aecda811d0e8fb4af72a1f39de89eac257
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-18 22:13:02 +0000

    removed weather forecast api call to forecast.js under utils folder

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const request = require('request');
-const geocode = require('./utils/geocode.js');
-// const url =
-//   'https://api.darksky.net/forecast/12583548f4b4cce45dfd01e54d5f60ff/37.8267,-122.4233';
-
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to weather service.');
-//   } else if (response.body.error) {
-//     console.log('Unable to find location.');
-//   } else {
-//     console.log(response.body.daily.data[0].summary);
-//   }
-// });
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 geocode('Philadelphia', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
+});
+
+forecast(-75.7088, 44.1545, (error, data) => {
   console.log('Error', error);
   console.log('Data', data);
 });
